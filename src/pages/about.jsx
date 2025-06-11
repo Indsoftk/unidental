1edf8a032565675a2cf8a2c012bac874b8a19971
themacmacka <themacmacka@gmail.com>
2025-06-11 13:32:09 -0600

    about changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import AboutSectionBanner from "./molecules/about/aboutSectionBanner"
+import AboutSectionBody from "./molecules/about/aboutSectionBody"
 
 const About = () => {
     return(
         <>
          <AboutSectionBanner/>
+         <AboutSectionBody/>
         </>
     )
 }
